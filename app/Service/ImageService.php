985736c95f19ae167ab32eff001e9c219de4421e
Stefan <stefan@studiokhi.com>
2025-01-09 17:20:29 +0100

    feat: change chmod uploads folder

--- app/Service/ImageService.php
+++ app/Service/ImageService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
@@ -13,26 +14,36 @@ class ImageService extends Controller
     public static function uploadImage(UploadedFile $image, $path, $name): array
     {
         $sizes = config('app.image_sizes');
-        chmod('uploads', 0755);
-        chmod('uploads/' . $path, 0755);
         if ($image->getMimeType() === 'video/mp4') {
             Storage::disk('uploads')->put($path . '/mp4/' . $name . '.mp4', file_get_contents($image));
-            chmod('uploads/' . $path . '/mp4/', 0755);
-            chmod('uploads/' . $path . '/mp4/' . $name . '.mp4', 0644);
             return ['mp4' => $name . '.mp4'];
         }
 
         $imageManager = new ImageManager(new Driver());
         $img = $imageManager->read($image)->toWebp(100);
         Storage::disk('uploads')->put($path . '/full/' . $name . '.webp', $img);
-        chmod('uploads/' . $path . '/full/', 0755);
-        chmod('uploads/' . $path . '/full/' . $name . '.webp', 0644);
         foreach ($sizes as $size) {
             $img = $imageManager->read($image)->scale($size)->toWebp(100);
             Storage::disk('uploads')->put($path . '/' . $size . '/' . $name . '.webp', $img);
-            chmod('uploads/' . $path . '/' . $size . '/', 0755);
-            chmod('uploads/' . $path . '/' . $size . '/' . $name . '.webp', 0644);
         }
+
+        // Appliquer les permissions au dossier principal
+        chmod('uploads', 0755);
+
+        // Parcourir récursivement les fichiers et dossiers
+        $files = File::allFiles('uploads');
+        $directories = File::directories('uploads');
+
+        // Appliquer les permissions aux fichiers
+        foreach ($files as $file) {
+            chmod($file->getRealPath(), 0644);
+        }
+
+        // Appliquer les permissions aux sous-dossiers
+        foreach ($directories as $dir) {
+            chmod($dir, 0755);
+        }
+
         return ['webp' => $name . '.webp'];
     }
 
